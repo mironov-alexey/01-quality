506e2308ecfeda8b982f9186f16da55f055f8334
Mironov Alexey <mironov.alexey.fiit.urfu@gmail.com>
2015-10-23 01:56:11 +0500

    Небольшие наброски, ничего конкретного

---
+++ MarkdownProcessor/Processor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MarkdownProcessor
+{
+    public class Processor
+    {
+        private readonly string _text;
+        private Dictionary<string, string> _markdown2openTags; 
+        private Dictionary<string, string> _markdown2closeTags;
+        public Processor(string text)
+        {
+            _text = text;
+            _markdown2openTags = new Dictionary<string, string>()
+            {
+                {"_",  "<em>"},
+                {"__",  "<strong>"},
+                {"`",  "<code>"}
+            };
+            _markdown2closeTags = new Dictionary<string, string>()
+            {
+                {"_", "</em>" },
+                {"__", "</strong>" },
+                {"`", "</code>" }
+            };
+        }
+
+        public bool IsOpeningTag(int index)
+        {
+            return Regex.IsMatch(_text.Substring(index + 1, 1), @"\w");
+            throw new NotImplementedException();
+        }
+
+        public bool IsClosingTag(int index)
+        {
+            return Regex.IsMatch(_text.Substring(index - 1, 1), @"\w");
+            throw new NotImplementedException();
+        }
+
+        private string GetHtmlView()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ MarkdownProcessor/Processor_Should.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MarkdownProcessor
+{
+    [TestFixture]
+    public class Processor_Should
+    {
+
+        private readonly string someText = "_a_";
+        private Processor processor;
+        [SetUp]
+        public void SetUp()
+        {
+            processor = new Processor(someText);
+        }
+
+        [Test]
+        public void Identify_CorrectOpeningTags()
+        {
+            var text = "_a";
+            var processor = new Processor(text);
+            Assert.IsTrue(processor.IsOpeningTag(0));
+        }
+
+        [Test]
+        public void Identify_CorrectClosingTags()
+        {
+            var text = "a_";
+            var processor = new Processor(text);
+            Assert.IsTrue(processor.IsClosingTag(2));
+        }
+
+        [Test]
+        public void NotIdentify_IncorrectOpeningTags()
+        {
+            var text = "_1";
+            var processor = new Processor(text);
+            Assert.IsFalse(processor.IsOpeningTag(0));
+        }
+
+        [Test]
+        public void NotIdentify_IncorrectClosingTags()
+        {
+            var text = "1_";
+            var processor = new Processor(text);
+            Assert.IsFalse(processor.IsOpeningTag(1));
+        }
+
+    }
+}
